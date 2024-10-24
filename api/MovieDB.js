9aa149301154705ac3da268a67bb81baa914db6a
Kien Tuong <trinhkientuong2003@gmail.com>
2024-10-24 11:16:11 +0700

    Create API calling Movies & Persons

--- api/MovieDB.js
+++ api/MovieDB.js
@@ -1,8 +1,16 @@
 import axios from "axios";
-import { apiKey } from "../constants";
-import {apiBaseUrl} from "../constants/index";
+import { apiKey, apiBaseUrl } from "../constants";
+
+export const movie_id = ""; //Mỗi lần cần sử dụng để lấy thông tin chi tiết phim cần import movie_id, gán giá trị id và gọi hàm fetchDetailsMovie
+export const person_id = "";
 //endpoints
-const detailsMovieEndpoint = `${apiBaseUrl}/movie/movie_id?api_key=${apiKey}`;
+const trendingMovieListEndpoint = `${apiBaseUrl}/trending/movie/day?api_key=${apiKey}`;
+const nowPlayingsMovieEndpoint = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+const upcomingMovieEndpoint = `${apiBaseUrl}/movie/upcoming?api_key=${apiKey}`;
+const detailsMovieEndpoint = `${apiBaseUrl}/movie/${movie_id}?api_key=${apiKey}`;
+const commentsMovieEndpoint = `${apiBaseUrl}/movie/${movie_id}/reviews?api_key=${apiKey}`;// Các đánh giá của bộ phim
+const creditsMovieEndpoint = `${apiBaseUrl}/movie/${movie_id}/credits?api_key=${apiKey}`;// Các diễn viên đóng bộ phim
+const personEndpoint = `${apiBaseUrl}/person/${person_id}s?api_key=${apiKey}`;
 
 const apiCall = async (endpoint, params) => {
   const options = {
@@ -19,9 +27,24 @@ const apiCall = async (endpoint, params) => {
   }
 };
 
-export const fetchTopRatedMovieList = () => {
-  return apiCall(topRatedMovieListEndpoint);
+export const fetchTrendingMovieList = () => {
+  return apiCall(trendingMovieListEndpoint);
 };
 export const fetchDetailsMovie = () => {
   return apiCall(detailsMovieEndpoint);
+};
+export const fetchNowPlayingsMovieEndpoint = () => {
+  return apiCall(nowPlayingsMovieEndpoint);
+};
+export const fetchUpcomingMovieEndpoint = () => {
+  return apiCall(upcomingMovieEndpoint);
+};
+export const fetchCommentsMovieEndpoint = () => {
+  return apiCall(commentsMovieEndpoint);
+};
+export const fetchCreditsMovieEndpoint = () => {
+  return apiCall(creditsMovieEndpoint);
+};
+export const fetchPersonEndpoint = () => {
+  return apiCall(personEndpoint);
 };
--- components/movieList.js
+++ components/movieList.js
@@ -1,59 +1,63 @@
-import { View, Text, Image, TouchableWithoutFeedback, Dimensions, ScrollView } from "react-native";
-import React from 'react'
+import {
+  View,
+  Text,
+  Image,
+  TouchableWithoutFeedback,
+  Dimensions,
+  ScrollView,
+} from "react-native";
+import React from "react";
 import Carousel from "react-native-snap-carousel";
 import { useNavigation } from "@react-navigation/native";
 import { TouchableOpacity } from "react-native";
-import { fetchTopRatedMovieList }from "../api/MovieDB";
-var { width, height } = Dimensions.get('window')
-
+import { fetchUpcomingMovieEndpoint } from "../api/MovieDB";
+var { width, height } = Dimensions.get("window");
 
 export default function MovieList({ title, data, hideSeeAll }) {
-    fetchTopRatedMovieList();
-    let movieName = 'SuperBatNman & sipderAquarium: infinity war'
-    const navigation = useNavigation();
-    return (
-        <View className="mb-8 space-y-4">
-            <View className="mx-4 flex-row justify-between items-center">
-                <Text className="text-white text-xl">{title}</Text>
-                {
-                    !hideSeeAll && (
-                        <TouchableOpacity>
-                            <Text className="text-customOrange text-lg">See All</Text>
-                        </TouchableOpacity>
-                    )
-                }
-            </View>
-            {/* movie row */}
-            <ScrollView
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                contentContainerStyle={{ paddingBottom: 15 }}
+  fetchUpcomingMovieEndpoint();
+  let movieName = "SuperBatNman & sipderAquarium: infinity war";
+  const navigation = useNavigation();
+  return (
+    <View className="mb-8 space-y-4">
+      <View className="mx-4 flex-row justify-between items-center">
+        <Text className="text-white text-xl">{title}</Text>
+        {!hideSeeAll && (
+          <TouchableOpacity>
+            <Text className="text-customOrange text-lg">See All</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+      {/* movie row */}
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 15 }}
+      >
+        {data.map((item, index) => {
+          return (
+            <TouchableWithoutFeedback
+              key={index}
+              onPress={() => navigation.push("Movie", item)}
             >
-                {
-                    data.map((item, index) => {
-                        return (
-                            <TouchableWithoutFeedback
-                                key={index}
-                                onPress={() => navigation.push('Movie', item)}
-                            >
-                                <View className="space-y-1 mr-4">
-                                    <Image
-                                        source={require('../assets/img/ava1.png')}
-                                        style={{
-                                            width: width * 0.5,
-                                            height: height * 0.32
-                                        }}
-                                        className="rounded-3xl"
-                                    />
-                                    <Text className="text-neutral-300 ml-1">
-                                        {movieName.length > 14 ? movieName.slice(0, 14) + '...' : movieName}
-                                    </Text>
-                                </View>
-                            </TouchableWithoutFeedback>
-                        )
-                    })
-                }
-            </ScrollView>
-        </View>
-    )
+              <View className="space-y-1 mr-4">
+                <Image
+                  source={require("../assets/img/ava1.png")}
+                  style={{
+                    width: width * 0.5,
+                    height: height * 0.32,
+                  }}
+                  className="rounded-3xl"
+                />
+                <Text className="text-neutral-300 ml-1">
+                  {movieName.length > 14
+                    ? movieName.slice(0, 14) + "..."
+                    : movieName}
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
 }
